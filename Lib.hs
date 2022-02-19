f88a155677400f4eb1718170c401e16e63d51c6e
Patrik Nemeth <patriknemethpn@gmail.com>
2022-02-19 14:35:40 +0100

    Add new type for the NKA

--- Lib.hs
+++ Lib.hs
@@ -9,11 +9,30 @@ module Lib where
 
 import System.IO
 
+-- Accessors for a triple
+fst3 :: (a, b, c) -> a
+fst3 (x, _, _) = x
+
+snd3 :: (a, b, c) -> b
+snd3 (_, x, _) = x
+
+thr3 :: (a, b, c) -> c
+thr3 (_, _, x) = x
+
 -- PLG == Prava Linearni Gramatika
 -- For the sake of consistency, this will not be translated.
 data PLG = PLG {
     nterms :: [Char], -- Non-terminals
     terms :: [Char],  -- Terminals
-    start :: Char,   -- Starting non-terminal
+    start :: Char,    -- Starting non-terminal
     rules :: [(Char, [Char])]   -- Rules of the PLG
 } deriving (Show)
+
+-- NKA == Nedeterministicky Konecny Automat
+data NKA = NKA {
+    states :: [Int],    -- States of the state automata
+    symbols :: [Char],  -- Alphabet symbols
+    startState :: Int,  -- Starting state
+    endStates :: [Int], -- End states
+    transitions :: [(Int, Char, Int)] -- Transition rules
+}
